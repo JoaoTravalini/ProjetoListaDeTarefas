43c6f92a8dfc2e25351dd8a71d0b78326f1b00f2
Joao Travalini <joao.m.travalini@gmail.com>
2024-08-08 22:25:19 -0300

    Criando estrutura finalizado

--- build/index.js
+++ build/index.js
@@ -2,3 +2,25 @@
 let listElement = document.querySelector("#app ul");
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
+let tarefas = [];
+function listarTarefas() {
+    listElement.innerHTML = "";
+    tarefas.map(item => {
+    });
+}
+function adicionarTarefa() {
+    if (inputElement.value === "") {
+        alert("Digite alguma tarefa!");
+        return false;
+    }
+    else {
+        let tarefaDigitada = inputElement.value;
+        tarefas.push(tarefaDigitada);
+        inputElement.value = "";
+        salvarDados();
+    }
+}
+buttonElement.onclick = adicionarTarefa;
+function salvarDados() {
+    localStorage.setItem("@listagem_tarefas", JSON.stringify(tarefas));
+}
