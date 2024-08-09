7c192087bf446aeb7b823159cc36114a456f6b4e
Joao Travalini <joao.m.travalini@gmail.com>
2024-08-08 22:39:02 -0300

    Buscando nossa lista finalizado e 3 minutos do Deletando tarefa

--- build/index.js
+++ build/index.js
@@ -2,12 +2,18 @@
 let listElement = document.querySelector("#app ul");
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
-let tarefas = [];
+let listaSalva = localStorage.getItem("@listagem_tarefas");
+let tarefas = listaSalva !== null && JSON.parse(listaSalva) || [];
 function listarTarefas() {
     listElement.innerHTML = "";
     tarefas.map(item => {
+        let todoElement = document.createElement("li");
+        let tarefaText = document.createTextNode(item);
+        todoElement.appendChild(tarefaText);
+        listElement.appendChild(todoElement);
     });
 }
+listarTarefas();
 function adicionarTarefa() {
     if (inputElement.value === "") {
         alert("Digite alguma tarefa!");
@@ -17,6 +23,7 @@ function adicionarTarefa() {
         let tarefaDigitada = inputElement.value;
         tarefas.push(tarefaDigitada);
         inputElement.value = "";
+        listarTarefas();
         salvarDados();
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,34 @@ let listElement = document.querySelector("#app ul") as HTMLUListElement;
 let inputElement = document.querySelector("#app input") as HTMLInputElement;
 let buttonElement = document.querySelector("#app button") as HTMLElement;
 
-let tarefas: string[] = [];
+let listaSalva: (string | null) = localStorage.getItem("@listagem_tarefas")
+let tarefas: string[] = listaSalva !== null && JSON.parse(listaSalva) || [];
 
 function listarTarefas(){
     listElement.innerHTML = "";
 
     tarefas.map( item => {
-        
+        let todoElement = document.createElement("li");
+        let tarefaText = document.createTextNode(item);
+
+        todoElement.appendChild(tarefaText);
+        listElement.appendChild(todoElement);
     })
 }
+
+listarTarefas();
 
 function adicionarTarefa() {
     if(inputElement.value === ""){
         alert("Digite alguma tarefa!")
         return false;
     }else{
+
         let tarefaDigitada: string = inputElement.value;
         tarefas.push(tarefaDigitada);
+
         inputElement.value = "";
+        listarTarefas();
         salvarDados();
     }
 }
